9d9af29f26ee46c54350f2db8da13cae52a3a775
Bappi Kabir <kabirbapson@gmail.com>
2023-03-13 22:06:09 +0100

    new Designn

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,23 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <>
+      <VStack  bg={'white'} borderRadius={"3xl"} alignItems={'flex-start'} >
+        <Image borderRadius={"2xl"} src={imageSrc} />
+        <Heading p={2} color={'black'} size={'md'}>
+          {title}
+        </Heading>
+        <Text p={2} color={'gray'} >
+          {description}
+        </Text>
+        <HStack p={2} pb={2} color={'black'} >
+          <Text>See more  </Text>
+          <FontAwesomeIcon size="1x" icon={faArrowRight} />
+        </HStack>
+      </VStack>
+    </>
+  )
 };
 
 export default Card;
